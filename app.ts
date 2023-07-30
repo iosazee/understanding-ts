f45c1d58d92ebb5b9bb9b8f5322625efca7ef1bd
Zee Mudia <iosazee1@gmail.com>
2023-07-30 12:47:33 +0100

    object type in ts

--- app.ts
+++ app.ts
@@ -1,15 +1,6 @@
-function add(num1: number, num2: number, showResult: boolean, phrase: string) {
-    const result = num1 + num2
-    if(showResult){
-        console.log(phrase + result)
-    } else {
-        return result
-    }
+const person: {name: string; age: number;} = {
+    name: "Sam",
+    age: 29
 }
 
-const n1 = 5
-const n2 = 4
-const printResult = true
-const resultPhrase = 'Reesult is: '
-
-const result = add(n1, n2, printResult, resultPhrase)
+console.log(person.name);
