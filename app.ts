eac9fde6c9e6890228fd558123b65d6f9b41eb94
Zee Mudia <iosazee1@gmail.com>
2023-07-30 10:55:41 +0100

    showing core data types in ts

--- app.ts
+++ app.ts
@@ -1,9 +1,15 @@
-function add(num1: number, num2: number) {
-    return num1 + num2
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
+    const result = num1 + num2
+    if(showResult){
+        console.log(phrase + result)
+    } else {
+        return result
+    }
 }
 
 const n1 = 5
 const n2 = 4
+const printResult = true
+const resultPhrase = 'Reesult is: '
 
-const result = add(n1, n2)
-console.log(result)
+const result = add(n1, n2, printResult, resultPhrase)
