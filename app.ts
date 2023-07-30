d22baffb68ef19786fdaa0d577597ba3ff80f942
Zee Mudia <iosazee1@gmail.com>
2023-07-30 14:04:34 +0100

    type tuple

--- app.ts
+++ app.ts
@@ -1,14 +1,19 @@
-const person = {
+const person: {name: string; age: number; hobbies: string[]; role: [number, string]} = {
     name: "Sam",
     age: 29,
-    hobbies: ['hiking', 'coding']
+    hobbies: ['hiking', 'coding'],
+    role: [2, 'author']
 }
 
 console.log(person);
 
-let favouriteActivities: string[];
-favouriteActivities = ['reading'];
+// let favouriteActivities: string[];
+// favouriteActivities = ['reading'];
 
-for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase())
-}
+// for (const hobby of person.hobbies) {
+//     console.log(hobby.toUpperCase())
+// }
+
+// person.role.push('admin');
+// person.role[1] = 10
+// person.role = [0, 'admin', 'user']
