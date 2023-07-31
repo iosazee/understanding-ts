ca5f00173a415e1cf1bcbbce5591f03c1f224909
Zee Mudia <iosazee1@gmail.com>
2023-07-31 10:37:42 +0100

    union type in ts

--- app.ts
+++ app.ts
@@ -1,33 +1,18 @@
-// const person: {name: string; age: number; hobbies: string[]; role: [number, string]} = {
-//     name: "Sam",
-//     age: 29,
-//     hobbies: ['hiking', 'coding'],
-//     role: [2, 'author']
-// }
+function combine(input1: number | string, input2: string | number) {
 
-// console.log(person);
+    let result;
+    if(typeof input1 === 'number'  && typeof input2 === 'number') {
+        result = input1 + input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
 
-// let favouriteActivities: string[];
-// favouriteActivities = ['reading'];
-
-// for (const hobby of person.hobbies) {
-//     console.log(hobby.toUpperCase())
-// }
-
-// person.role.push('admin');
-// person.role[1] = 10
-// person.role = [0, 'admin', 'user']
-
-enum Role {ADMIN, READ_ONLY, AUTHOR}
-
-const person = {
-    name: 'Sam',
-    age: 29,
-    hobbies: ['cooking', 'coding'],
-    role: Role.AUTHOR
+    return result
 }
 
-if (person.role === Role.AUTHOR) {
-    console.log(person.name, 'is an author')
-}
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine('Max', 'Pam');
+console.log(combinedNames);
 
