6159b1fa532ef227d397b4899d433d6b60824173
Zee Mudia <iosazee1@gmail.com>
2023-07-31 01:45:39 +0100

    the enum type in ts

--- app.ts
+++ app.ts
@@ -1,11 +1,11 @@
-const person: {name: string; age: number; hobbies: string[]; role: [number, string]} = {
-    name: "Sam",
-    age: 29,
-    hobbies: ['hiking', 'coding'],
-    role: [2, 'author']
-}
+// const person: {name: string; age: number; hobbies: string[]; role: [number, string]} = {
+//     name: "Sam",
+//     age: 29,
+//     hobbies: ['hiking', 'coding'],
+//     role: [2, 'author']
+// }
 
-console.log(person);
+// console.log(person);
 
 // let favouriteActivities: string[];
 // favouriteActivities = ['reading'];
@@ -17,3 +17,17 @@ console.log(person);
 // person.role.push('admin');
 // person.role[1] = 10
 // person.role = [0, 'admin', 'user']
+
+enum Role {ADMIN, READ_ONLY, AUTHOR}
+
+const person = {
+    name: 'Sam',
+    age: 29,
+    hobbies: ['cooking', 'coding'],
+    role: Role.AUTHOR
+}
+
+if (person.role === Role.AUTHOR) {
+    console.log(person.name, 'is an author')
+}
+
